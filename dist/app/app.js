83e66075c092be1ad7304b8c8811dfa12499dc97
Mehebul02 <mehebul2123@gmail.com>
2024-11-16 00:06:05 +0600

    adde the data id

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,7 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('I am serving bdCalling It Ltd as a front end developer');
+});
+app.get('/', (req, res) => {
+    console.log(req.body);
+    res.json({
+        success: true,
+        message: 'Confirm the data'
+    });
+});
+app.get('/userId', (req, res) => {
+    console.log(req.params);
+    res.json({
+        success: true,
+        message: 'Data successfully get'
+    });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,28 @@
+
 import express, { Request, Response } from 'express'
 const app = express()
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('I am serving bdCalling It Ltd as a front end developer')
+})
+
+app.get('/', (req: Request, res: Response) => {
+    console.log(req.body);
+
+    res.json({
+        success: true,
+        message: 'Confirm the data'
+    })
+})
+
+app.get('/userId', (req: Request, res: Response) => {
+    console.log(req.params);
+    res.json({
+        success: true,
+        message: 'Data successfully get'
+    })
 })
 
 
